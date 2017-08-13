aa3e90c403b6c35a766919fe8aae9baa442c38bc
Danny Ben Shitrit <dannyb@telesofia.com>
2017-08-13 17:01:21 +0000

    client full spec coverage

--- spec/load_runner/client_spec.rb
+++ spec/load_runner/client_spec.rb
@@ -19,10 +19,37 @@ describe Client do
         subject.send :ping, ref: 'refs/heads/test', repo: 'myrepo'
       end
     end
+
+    context "with branch argument" do
+      it "converts it to ref" do
+        expected = { ref: "refs/heads/branchy" }
+        expect(subject).to receive(:send_payload).with(anything, hash_including(expected))
+        subject.send(:push, branch: 'branchy')
+      end
+    end
+
+    context "with tag argument" do
+      it "converts it to ref" do
+        expected = { ref: "refs/tags/tagly" }
+        expect(subject).to receive(:send_payload).with(anything, hash_including(expected))
+        subject.send(:push, tag: 'tagly')
+      end
+    end
+
+    context "with a full name repo" do
+      it "extracts its short name" do
+        expected = { repository: { name: "myrepo", full_name: 'me/myrepo' } }
+        expect(subject).to receive(:send_payload).with(anything, hash_including(expected))
+        subject.send(:push, repo: 'me/myrepo')
+      end
+    end
+
+
+
   end
 
   describe "#send_payload" do
-    let(:payload) { {:ref=>'refs/heads/test', :repository=>{:name=>'myrepo'}} }
+    let(:payload) { {ref: 'refs/heads/test', repository: {name: 'myrepo'}} }
     
     it "sends a post http message to /payload" do
       expect(described_class).to receive(:post).with('/payload', any_args)
@@ -33,6 +60,23 @@ describe Client do
       expected = { body: payload.to_json, headers: { "X_GITHUB_EVENT"=>"push" } }
       expect(described_class).to receive(:post).with(anything, expected)
       subject.send_payload(:push, payload)
+    end
+
+    context "when secret_token is set" do
+      before do
+        subject.secret_token = '123'
+      end
+
+      it "sends a signature in the header" do
+        expected = { 
+          headers: { 
+            "X_GITHUB_EVENT"  => "push",
+            "X_HUB_SIGNATURE" => "sha1=f2d099c2ff67f1f52e1a0b9e8445306e1d30e6e4"
+          },
+        }
+        expect(described_class).to receive(:post).with(anything, hash_including(expected))
+        subject.send_payload(:push, payload)
+      end
     end
   end
 end
