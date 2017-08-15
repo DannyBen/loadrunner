7a4ebb8c42766a99abf4c866bceff1e13227add4
Danny Ben Shitrit <dannyb@telesofia.com>
2017-08-15 05:06:54 +0000

    complete server spec

--- spec/load_runner/server_spec.rb
+++ spec/load_runner/server_spec.rb
@@ -14,5 +14,22 @@ describe Server do
       expect_any_instance_of(Runner).to receive(:execute)
       post '/payload', payload
     end
+
+    context "when a signature is required" do
+      before do
+        ENV['GITHUB_SECRET_TOKEN'] = '123'
+      end
+
+      after do
+        ENV['GITHUB_SECRET_TOKEN'] = nil
+      end
+
+      it "halts with 401" do
+        expect_any_instance_of(Runner).not_to receive(:execute)
+        post '/payload', payload
+        expect(last_response.status).to be 401
+        expect(last_response.body).to eq halt_messages[:no_client]
+      end
+    end
   end
 end
